4cfe05841f33d643a2007428a247cb77cd21c4ed
Junior Martins <juniorsoaresmartins@gmail.com>
2023-05-24 17:37:51 -0400

    FEAT: configuração de application e database.

---
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/model/Cliente.java
@@ -0,0 +1,43 @@
+package com.techchallenge.devnet.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "clientes")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"id"})
+public final class Cliente implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "codigo", nullable = false)
+  private String codigo;
+
+  @Column(name = "nome", length = 100, nullable = false)
+  private String nome;
+}
+
