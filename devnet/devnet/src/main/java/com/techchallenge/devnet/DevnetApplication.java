a21e1b7e8f228dce0096e2e2bfdfcdad617ae9c7
Junior Martins <juniorsoaresmartins@gmail.com>
2023-05-24 07:39:38 -0400

    FEAT: adicionado start do projeto.

---
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/DevnetApplication.java
@@ -0,0 +1,13 @@
+package com.techchallenge.devnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevnetApplication.class, args);
+	}
+
+}
---
+++ devnet/devnet/src/test/java/com/techchallenge/devnet/DevnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.devnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
