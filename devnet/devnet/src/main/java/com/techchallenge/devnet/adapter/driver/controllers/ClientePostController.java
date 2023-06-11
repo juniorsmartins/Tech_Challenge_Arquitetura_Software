e414bde68dda4033b41b5dca53fa56b50473a547
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-11 11:54:54 -0400

    DOCS: add dependência SpringDoc e anotações em ClientePostController.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/controllers/ClientePostController.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/controllers/ClientePostController.java
@@ -3,6 +3,14 @@ package com.techchallenge.devnet.adapter.driver.controllers;
 import com.techchallenge.devnet.adapter.driver.dtos.request.ClienteDtoRequest;
 import com.techchallenge.devnet.adapter.driver.dtos.response.ClienteDtoResponse;
 import com.techchallenge.devnet.core.application.use_case.IClienteService;
+import com.techchallenge.devnet.core.domain.base.exceptions.RetornoDeErro;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Tag(name = "ClientePostController", description = "Adaptador para criação do recurso Cliente.")
 @Slf4j
 @RestController
 @RequestMapping(path = "/api/v1/clientes")
@@ -20,9 +29,20 @@ public final class ClientePostController implements IClienteController.PostContr
   @Autowired
   private IClienteService.CadastrarService service;
 
+  @Operation(summary = "Cadastrar Cliente", description = "Este recurso destina-se a cadastrar.")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "Created - novo recurso criado com sucesso!", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ClienteDtoRequest.class))}),
+    @ApiResponse(responseCode = "400", description = "Bad Request - requisição mal feita.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "401", description = "Unauthorized: cliente não possui autorização para acessar recurso.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "403", description = "Forbidden - cliente autenticado com sucesso, mas sem permissões para acessar recurso.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "409", description = "Conflict - requisição não concluída por conflito de estado do recurso.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "500", description = "Internal Server Error - situação inesperada no servidor.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))})
+  })
   @Override
-  public ResponseEntity<ClienteDtoResponse> cadastrar(@RequestBody @Valid final ClienteDtoRequest dtoRequest,
-                                                      final UriComponentsBuilder uriComponentsBuilder) {
+  public ResponseEntity<ClienteDtoResponse> cadastrar(
+    @Parameter(name = "ClienteDtoRequest", description = "Estrutura de dados para transporte de informações de entrada.", required = true)
+    @RequestBody @Valid final ClienteDtoRequest dtoRequest, final UriComponentsBuilder uriComponentsBuilder) {
+
     var response = this.service.cadastrar(dtoRequest);
 
     return ResponseEntity
