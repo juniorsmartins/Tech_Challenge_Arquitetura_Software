252d5acd94a9a7ef72251c28a22d0fd2eeb36803
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-13 06:10:31 -0400

    REFACTOR: ajuste em DTO. Transformação em Builder.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/dtos/request/ItemPedidoDtoRequest.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/dtos/request/ItemPedidoDtoRequest.java
@@ -4,11 +4,17 @@ import com.techchallenge.devnet.adapter.driver.dtos.ProdutoDtoResumo;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public final class ItemPedidoDtoRequest implements Serializable {
