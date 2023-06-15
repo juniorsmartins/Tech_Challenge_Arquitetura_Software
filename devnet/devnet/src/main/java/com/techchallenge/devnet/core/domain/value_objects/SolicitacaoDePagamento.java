3fc5845378451886c54115ef731a7d525bc02779
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-15 18:48:52 -0400

    FEAT: construção do pagamento.

---
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/value_objects/SolicitacaoDePagamento.java
@@ -0,0 +1,24 @@
+package com.techchallenge.devnet.core.domain.value_objects;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.techchallenge.devnet.adapter.driver.dtos.response.PagamentoDtoResponse;
+import com.techchallenge.devnet.adapter.driver.dtos.response.PedidoDtoResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public final class SolicitacaoDePagamento {
+
+  private PedidoDtoResponse pedido;
+
+  private PagamentoDtoResponse pagamento;
+}
+
