6d36a68da22fa907fb55b7eff28f544389d556c2
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-18 08:49:57 -0400

    FEAT: anotação personalizada para limitar tamanho de arquivo de foto. É alternativa para a limitação no application.properties.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/dtos/request/FotoProdutoDtoRequest.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/dtos/request/FotoProdutoDtoRequest.java
@@ -1,5 +1,6 @@
 package com.techchallenge.devnet.adapter.driver.dtos.request;
 
+import com.techchallenge.devnet.core.domain.base.anotacoes.FileSize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -21,6 +22,7 @@ public final class FotoProdutoDtoRequest implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @NotNull
+  @FileSize(max = "2MB")
   private MultipartFile foto;
 
   @NotBlank
