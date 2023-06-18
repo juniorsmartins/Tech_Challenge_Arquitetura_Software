ea4d2301c038d1d06e8451b92443ebfb1dd9d587
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-18 15:38:54 -0400

    FEAT: armazenamento de arquivo de foto.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven/infra/armazem/FotoProdutoArmazemService.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven/infra/armazem/FotoProdutoArmazemService.java
@@ -1,8 +1,35 @@
 package com.techchallenge.devnet.adapter.driven.infra.armazem;
 
 import com.techchallenge.devnet.core.application.ports.IFotoProdutoArmazemService;
+import com.techchallenge.devnet.core.domain.base.exceptions.ArmazemException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
-public class FotoProdutoArmazemService implements IFotoProdutoArmazemService {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+@Service
+public final class FotoProdutoArmazemService implements IFotoProdutoArmazemService {
+
+  @Value("${devnet.armazem.local.diretorio-fotos}")
+  private Path caminhoDoDiretorio;
+
+  @Override
+  public void armazenar(NovaFoto novaFoto) {
+
+    try {
+      Path caminhoDoArquivo = this.pegarCaminhoDoArquivo(novaFoto.getNomeArquivo());
+      FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(caminhoDoArquivo));
+
+    } catch (IOException e) {
+      throw new ArmazemException("Não foi possível armazenar arquivo de foto.", e);
+    }
+  }
+
+  private Path pegarCaminhoDoArquivo(String nomeArquivo) {
+    return caminhoDoDiretorio.resolve(Path.of(nomeArquivo));
+  }
 }
 
--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/ports/IFotoProdutoArmazemService.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/ports/IFotoProdutoArmazemService.java
@@ -1,7 +1,19 @@
 package com.techchallenge.devnet.core.application.ports;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.InputStream;
+
 public interface IFotoProdutoArmazemService {
 
-  void armazenar();
+  void armazenar(NovaFoto novaFoto);
+
+  @Builder
+  @Getter
+  class NovaFoto {
+    private String nomeArquivo;
+    private InputStream inputStream;
+  }
 }
 
