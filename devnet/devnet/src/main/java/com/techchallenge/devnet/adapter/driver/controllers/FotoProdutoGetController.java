939ce36bcbb5b3cb12cd7421512839ecfee961cb
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-19 18:11:47 -0400

    FEAT: Consultar FotoProduto em construção.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/controllers/FotoProdutoGetController.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver/controllers/FotoProdutoGetController.java
@@ -1,6 +1,8 @@
 package com.techchallenge.devnet.adapter.driver.controllers;
 
 import com.techchallenge.devnet.adapter.driver.dtos.resposta.FotoProdutoDtoResponse;
+import com.techchallenge.devnet.core.application.use_case.IFotoProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/fotos")
 public class FotoProdutoGetController implements IFotoProdutoController.GetController {
 
+  @Autowired
+  private IFotoProdutoService.PesquisarService fotoProdutoPesquisarService;
+
   @Override
   public ResponseEntity<FotoProdutoDtoResponse> consultarPorId(@PathVariable(name = "id") final Long id) {
 
-    return null;
+    var response = this.fotoProdutoPesquisarService.consultarPorId(id);
+
+    return ResponseEntity
+      .ok()
+      .body(response);
   }
 }
 
--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/use_case/FotoProdutoGetService.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/use_case/FotoProdutoGetService.java
@@ -2,21 +2,30 @@ package com.techchallenge.devnet.core.application.use_case;
 
 import com.techchallenge.devnet.adapter.driver.dtos.resposta.FotoProdutoDtoResponse;
 import com.techchallenge.devnet.core.application.ports.IFotoProdutoRepository;
+import com.techchallenge.devnet.core.domain.base.exceptions.http_404.FotoProdutoNaoEncontradoException;
+import com.techchallenge.devnet.core.domain.base.mappers.IMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 public class FotoProdutoGetService implements IFotoProdutoService.PesquisarService {
 
   @Autowired
+  private IMapper mapper;
+
+  @Autowired
   private IFotoProdutoRepository.GetRepository fotoProdutoGetRepository;
 
+  @Transactional(readOnly = true)
   @Override
   public FotoProdutoDtoResponse consultarPorId(final Long id) {
 
-    return null;
+    return this.fotoProdutoGetRepository.consultarPorId(id)
+      .map(fotoProduto -> this.mapper.converterEntidadeParaDtoResponse(fotoProduto, FotoProdutoDtoResponse.class))
+      .orElseThrow(() -> new FotoProdutoNaoEncontradoException(id));
   }
 }
 
