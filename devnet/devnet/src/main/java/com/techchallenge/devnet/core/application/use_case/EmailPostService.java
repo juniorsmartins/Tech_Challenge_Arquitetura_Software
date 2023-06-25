7078ed899af2617e92c45529aa6054ae94c2f68e
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-25 10:18:16 -0400

    FEAT: concluída estrutura de envio de email.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/use_case/EmailPostService.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/use_case/EmailPostService.java
@@ -1,14 +1,19 @@
 package com.techchallenge.devnet.core.application.use_case;
 
+import com.techchallenge.devnet.adapter.driver_primario.conversores.IMapper;
 import com.techchallenge.devnet.adapter.driver_primario.dtos.requisicao.EmailDtoRequest;
 import com.techchallenge.devnet.adapter.driver_primario.dtos.resposta.EmailDtoResponse;
 import com.techchallenge.devnet.core.application.ports.entrada.IEmailService;
 import com.techchallenge.devnet.core.application.ports.saida.IEmailRepository;
-import com.techchallenge.devnet.adapter.driver_primario.conversores.IMapper;
 import com.techchallenge.devnet.core.domain.entities.Email;
+import com.techchallenge.devnet.core.domain.entities.enums.StatusEmailEnum;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -18,6 +23,9 @@ public class EmailPostService implements IEmailService.EnviarService {
   private IMapper mapper;
 
   @Autowired
+  private JavaMailSender javaMailSender;
+
+  @Autowired
   private IEmailRepository.PostRepository emailPostRepository;
 
   @Override
@@ -25,7 +33,26 @@ public class EmailPostService implements IEmailService.EnviarService {
 
     return Optional.of(dtoRequest)
       .map(dto -> this.mapper.converterDtoRequestParaEntidade(dto, Email.class))
-      .map(this.emailPostRepository::salvar)
+      .map(email -> {
+        email.setSendDataEmail(LocalDateTime.now());
+
+        try {
+          SimpleMailMessage mensagem = new SimpleMailMessage();
+          mensagem.setFrom(email.getEmailFrom());
+          mensagem.setTo(email.getEmailTo());
+          mensagem.setSubject(email.getSubject());
+          mensagem.setText(email.getText());
+          javaMailSender.send(mensagem);
+
+          email.setStatusEmail(StatusEmailEnum.SENT);
+        } catch (MailException mailException) {
+          email.setStatusEmail(StatusEmailEnum.ERROR);
+        } finally {
+          this.emailPostRepository.salvar(email);
+        }
+
+        return email;
+      })
       .map(email -> this.mapper.converterEntidadeParaDtoResponse(email, EmailDtoResponse.class))
       .orElseThrow();
   }
