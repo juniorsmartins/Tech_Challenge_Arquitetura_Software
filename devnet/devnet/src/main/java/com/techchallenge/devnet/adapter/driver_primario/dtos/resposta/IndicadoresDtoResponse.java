1e5edf39f7becb197718959e260c54a9957cac82
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-26 07:21:18 -0400

    FEAT: início da construção do painel administrativo.

---
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver_primario/dtos/resposta/IndicadoresDtoResponse.java
@@ -0,0 +1,32 @@
+package com.techchallenge.devnet.adapter.driver_primario.dtos.resposta;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public final class IndicadoresDtoResponse implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private BigDecimal totalPagamentoEmAberto;
+
+  private BigDecimal totalPagamentoPago;
+
+  private BigDecimal totalPagamentoCancelado;
+
+  private OffsetDateTime dataHoraDadosCriados = OffsetDateTime.now();
+}
+
