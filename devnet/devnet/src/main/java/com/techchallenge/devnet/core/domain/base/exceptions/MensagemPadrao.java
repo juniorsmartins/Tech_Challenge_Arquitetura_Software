e03b06d51fb2470955f8aefe6325c1cc96864ecc
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-03 06:08:31 -0400

    REFACTOR: ajuste em mensagem padrão.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/exceptions/MensagemPadrao.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/exceptions/MensagemPadrao.java
@@ -28,7 +28,7 @@ public final class MensagemPadrao {
 
   public static final String PAGAMENTO_NAO_ENCONTRADO = "O Pagamento, com id %s, não foi encontrado.";
 
-  public static final String PAGAMENTO_BLOQUEADO = "O Pedido, com id %s, teve confirmação de pagamento bloqueado por estar no status: %s.";
+  public static final String PAGAMENTO_BLOQUEADO = "O Pedido, com id %s, já obteve confirmação de pagamento.";
 
   public static final String PEDIDO_BLOQUEADO_PARA_ATUALIZAR = "O Pedido, com id %s, não pode ser atualizado por estar no status: %s.";
 
