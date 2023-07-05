6b53afd49873e8a9b08afdfecb7b21ac65d30027
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-05 06:10:50 -0400

    TEST: add em Produto.

--- devnet/devnet/src/test/java/com/techchallenge/devnet/adapter/driver_primario/controllers/ProdutoDeleteControllerIntegrationTest.java
+++ devnet/devnet/src/test/java/com/techchallenge/devnet/adapter/driver_primario/controllers/ProdutoDeleteControllerIntegrationTest.java
@@ -1,12 +1,27 @@
 package com.techchallenge.devnet.adapter.driver_primario.controllers;
 
 import com.techchallenge.devnet.DevnetApplication;
+import com.techchallenge.devnet.adapter.driven_secundario.entities.ProdutoEntity;
+import com.techchallenge.devnet.adapter.driven_secundario.repositorios.jpa.ProdutoRepositoryJpa;
+import com.techchallenge.devnet.utils.CriadorDeObjetos;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest(classes = DevnetApplication.class)
 @AutoConfigureMockMvc
@@ -14,46 +29,72 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ProdutoDeleteControllerIntegrationTest {
 
-//  public static final String END_POINT = "/api/v1/produtos";
-//
-//  public static final String UTF8 = "UTF-8";
-//
-//  @Autowired
-//  private MockMvc mockMvc;
-//
-//  @Autowired
-//  private ProdutoRepositoryJpa produtoRepositoryJpa;
-//
-//  @Test
-//  @Order(1)
-//  @DisplayName("Deletar - http 204")
-//  void deveRetornarHttp204_quandoDeletar() throws Exception {
-//
-//    var entidade = CriadorDeObjetos.gerarProdutoBuilder()
-//      .build();
-//    entidade = this.produtoRepositoryJpa.save(entidade);
-//
-//    this.mockMvc.perform(MockMvcRequestBuilders.delete(END_POINT.concat("/") + entidade.getId())
-//        .contentType(MediaType.APPLICATION_JSON)
-//        .characterEncoding(UTF8)
-//        .accept(MediaType.APPLICATION_JSON))
-//      .andExpect(MockMvcResultMatchers.status().isNoContent())
-//      .andDo(MockMvcResultHandlers.print());
-//  }
-//
-//  @Test
-//  @Order(5)
-//  @DisplayName("Deletar - http 404 por id inexistente")
-//  void deveRetornarHttp404_quandoDeletarComIdInexistente() throws Exception {
-//
-//    var idInexistente = Math.round((Math.random() + 1) * 100000);
-//
-//    this.mockMvc.perform(MockMvcRequestBuilders.delete(END_POINT.concat("/") + idInexistente)
-//        .contentType(MediaType.APPLICATION_JSON)
-//        .characterEncoding(UTF8)
-//        .accept(MediaType.APPLICATION_JSON))
-//      .andExpect(MockMvcResultMatchers.status().isNotFound())
-//      .andDo(MockMvcResultHandlers.print());
-//  }
+  public static final String END_POINT = "/api/v1/produtos";
+
+  public static final String UTF8 = "UTF-8";
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ProdutoRepositoryJpa produtoRepositoryJpa;
+
+  private ProdutoEntity produtoEntity;
+
+  @BeforeEach
+  void criadorDeCenarios() {
+    produtoEntity = CriadorDeObjetos.gerarProdutoEntityBuilder()
+      .build();
+    produtoEntity = this.produtoRepositoryJpa.save(produtoEntity);
+  }
+
+  @AfterEach
+  void destruidorDeCenarios() {
+    this.produtoRepositoryJpa.deleteAll();
+  }
+
+  @Test
+  @Order(1)
+  @DisplayName("Deletar - http 204")
+  void deveRetornarHttp204_quandoDeletar() throws Exception {
+
+    this.mockMvc.perform(MockMvcRequestBuilders.delete(END_POINT.concat("/") + produtoEntity.getId())
+        .contentType(MediaType.APPLICATION_JSON)
+        .characterEncoding(UTF8)
+        .accept(MediaType.APPLICATION_JSON))
+      .andExpect(MockMvcResultMatchers.status().isNoContent())
+      .andDo(MockMvcResultHandlers.print());
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("Deletar - http 204 e apagar no banco")
+  void deveApagarNoBanco_quandoDeletar() throws Exception {
+
+    this.mockMvc.perform(MockMvcRequestBuilders.delete(END_POINT.concat("/") + produtoEntity.getId())
+        .contentType(MediaType.APPLICATION_JSON)
+        .characterEncoding(UTF8)
+        .accept(MediaType.APPLICATION_JSON))
+      .andExpect(MockMvcResultMatchers.status().isNoContent())
+      .andDo(MockMvcResultHandlers.print());
+
+    var existe = this.produtoRepositoryJpa.existsById(produtoEntity.getId());
+    Assertions.assertEquals(false, existe);
+  }
+
+  @Test
+  @Order(5)
+  @DisplayName("Deletar - http 404 por id inexistente")
+  void deveRetornarHttp404_quandoDeletarComIdInexistente() throws Exception {
+
+    var idInexistente = Math.round((Math.random() + 1) * 100000);
+
+    this.mockMvc.perform(MockMvcRequestBuilders.delete(END_POINT.concat("/") + idInexistente)
+        .contentType(MediaType.APPLICATION_JSON)
+        .characterEncoding(UTF8)
+        .accept(MediaType.APPLICATION_JSON))
+      .andExpect(MockMvcResultMatchers.status().isNotFound())
+      .andDo(MockMvcResultHandlers.print());
+  }
 }
 
