53374e0ffa1b54584c2be20f4012135772428923
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-10 07:39:05 -0400

    FEAT: add swagger.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver_primario/controllers/PedidoGetControllerAdapter.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driver_primario/controllers/PedidoGetControllerAdapter.java
@@ -63,6 +63,15 @@ public final class PedidoGetControllerAdapter implements IPedidoControllerPort.G
       .body(response);
   }
 
+  @Operation(summary = "Listar Pedido Ordenado", description = "Este recurso permite listar Pedido ordenado por StatusPedido e por dataHora do recebimento.")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "OK - requisição bem sucedida e com retorno.", content = {@Content(mediaType = "application/json", array = @ArraySchema(minItems = 1, schema = @Schema(implementation = PedidoFiltro.class), uniqueItems = true))}),
+    @ApiResponse(responseCode = "400", description = "Bad Request - requisição mal feita.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "401", description = "Unauthorized: cliente não autenticado.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "403", description = "Forbidden - cliente autenticado, mas sem autorização para acessar recurso.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "404", description = "Not Found - recurso não encontrado no banco de dados.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))}),
+    @ApiResponse(responseCode = "500", description = "Internal Server Error - situação inesperada no servidor.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = RetornoDeErro.class))})
+  })
   @Override
   public ResponseEntity<List<PedidoDtoResponse>> listarOrdenadoPorStatusAndDataHoraCadastro() {
 
