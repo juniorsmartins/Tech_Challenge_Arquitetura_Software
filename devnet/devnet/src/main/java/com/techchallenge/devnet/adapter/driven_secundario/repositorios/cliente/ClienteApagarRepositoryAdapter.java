8bc1d630b234a0fb30dcc561c3385e7c52c32c51
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-11 06:27:35 -0400

    REFACTOR: segregação de interfaces.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven_secundario/repositorios/cliente/ClienteApagarRepositoryAdapter.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven_secundario/repositorios/cliente/ClienteApagarRepositoryAdapter.java
@@ -3,7 +3,7 @@ package com.techchallenge.devnet.adapter.driven_secundario.repositorios;
 import com.techchallenge.devnet.adapter.driven_secundario.conversores_saida.IMapperSaida;
 import com.techchallenge.devnet.adapter.driven_secundario.entities.ClienteEntity;
 import com.techchallenge.devnet.adapter.driven_secundario.repositorios.jpa.ClienteRepositoryJpa;
-import com.techchallenge.devnet.core.application.ports.saida.IClienteRepositoryPort;
+import com.techchallenge.devnet.core.application.ports.saida.cliente.IClienteApagarRepositoryPort;
 import com.techchallenge.devnet.core.domain.models.ClienteModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Repository
-public class ClienteDeleteRepositoryAdapter implements IClienteRepositoryPort.DeleteRepository {
+public class ClienteApagarRepositoryAdapter implements IClienteApagarRepositoryPort {
 
   @Autowired
   private IMapperSaida mapper;
