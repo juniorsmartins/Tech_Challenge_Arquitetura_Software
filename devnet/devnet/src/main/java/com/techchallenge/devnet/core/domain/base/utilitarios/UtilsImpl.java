05b003eaa598910bdcacd27917ac71acdc9eaa8b
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-12 05:17:18 -0400

    REFACTOR: segregação de interfaces.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/utilitarios/UtilsImpl.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/utilitarios/UtilsImpl.java
@@ -3,6 +3,7 @@ package com.techchallenge.devnet.core.domain.base.utilitarios;
 import com.techchallenge.devnet.core.application.ports.entrada.IEmailServicePort;
 import com.techchallenge.devnet.core.application.ports.saida.cliente.IClienteConsultarPorCpfRepositoryPort;
 import com.techchallenge.devnet.core.application.ports.saida.cliente.IClienteConsultarPorIdRepositoryPort;
+import com.techchallenge.devnet.core.application.ports.saida.produto.IProdutoConsultarPorIdRepositoryPort;
 import com.techchallenge.devnet.core.domain.base.exceptions.MensagemPadrao;
 import com.techchallenge.devnet.core.domain.base.exceptions.http_404.ClienteNaoEncontradoException;
 import com.techchallenge.devnet.core.domain.base.exceptions.http_404.ProdutoNaoEncontradoException;
@@ -24,7 +25,7 @@ public final class UtilsImpl implements IUtils {
   private IClienteConsultarPorCpfRepositoryPort clienteConsultarPorCpfRepository;
 
   @Autowired
-  private IProdutoRepositoryPort.GetRepository produtoGetRepository;
+  private IProdutoConsultarPorIdRepositoryPort produtoConsultarPorIdRepository;
 
   @Autowired
   private IEmailServicePort.EnviarService emailEnviarService;
@@ -54,7 +55,7 @@ public final class UtilsImpl implements IUtils {
 
     pedidoModel.getItensPedido().forEach(item -> {
       var idProduto = item.getProduto().getId();
-      var produto = this.produtoGetRepository.consultarPorId(idProduto)
+      var produto = this.produtoConsultarPorIdRepository.consultarPorId(idProduto)
         .orElseThrow(() -> new ProdutoNaoEncontradoException(idProduto));
       item.setProduto(produto);
     });
