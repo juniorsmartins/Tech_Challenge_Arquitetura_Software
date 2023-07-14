b9b1ce709a0e2377f7006001b122ea43d8e89b8c
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-14 19:39:40 -0400

    FEAT: segregação de interfaces.

---
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven_secundario/repositorios/pagamento/PagamentoPesquisarRepositoryAdapter.java
@@ -0,0 +1,37 @@
+package com.techchallenge.devnet.adapter.driven_secundario.repositorios.pagamento;
+
+import com.techchallenge.devnet.adapter.driven_secundario.conversores_saida.IMapperSaida;
+import com.techchallenge.devnet.adapter.driver_primario.dtos.filtros.PagamentoFiltroDto;
+import com.techchallenge.devnet.core.application.ports.saida.pagamento.IPagamentoPesquisarRepositoryPort;
+import com.techchallenge.devnet.core.domain.models.PagamentoModel;
+import com.techchallenge.devnet.core.domain.objects.filtros.PagamentoFiltro;
+import com.techchallenge.devnet.core.domain.objects.specification.PagamentoSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public class PagamentoPesquisarRepositoryAdapter implements IPagamentoPesquisarRepositoryPort {
+
+  @Autowired
+  private IMapperSaida mapper;
+
+  @Autowired
+  private PagamentoRepositoryJpa jpa;
+
+  @Transactional(readOnly = true)
+  @Override
+  public Page<PagamentoModel> pesquisar(final PagamentoFiltro pagamentoFiltro, final Pageable paginacao) {
+
+    return Optional.of(pagamentoFiltro)
+      .map(filtro -> this.mapper.converterOrigemParaDestino(filtro, PagamentoFiltroDto.class))
+      .map(filtroDto -> this.jpa.findAll(PagamentoSpecification.consultarDinamicamente(filtroDto), paginacao))
+      .map(paginaEntity -> this.mapper.converterPaginaOrigemParaPaginaDestino(paginaEntity, PagamentoModel.class))
+      .orElseThrow();
+  }
+}
+
--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/use_case/PagamentoPesquisarService.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/application/use_case/PagamentoPesquisarService.java
@@ -1,15 +1,11 @@
 package com.techchallenge.devnet.core.application.use_case;
 
-import com.techchallenge.devnet.core.application.ports.entrada.IPagamentoServicePort;
-import com.techchallenge.devnet.core.application.ports.saida.IPagamentoRepositoryPort;
-import com.techchallenge.devnet.core.domain.base.exceptions.MensagemPadrao;
-import com.techchallenge.devnet.core.domain.base.exceptions.http_404.PagamentoNaoEncontradoException;
-import com.techchallenge.devnet.core.domain.base.utilitarios.IQRCodeGenerator;
+import com.techchallenge.devnet.core.application.ports.entrada.pagamento.IPagamentoPesquisarServicePort;
+import com.techchallenge.devnet.core.application.ports.saida.pagamento.IPagamentoPesquisarRepositoryPort;
 import com.techchallenge.devnet.core.domain.models.PagamentoModel;
 import com.techchallenge.devnet.core.domain.objects.filtros.PagamentoFiltro;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -18,34 +14,17 @@ import java.util.Optional;
 
 @Slf4j
 @Service
-public class PagamentoGetService implements IPagamentoServicePort.GetService {
+public class PagamentoPesquisarService implements IPagamentoPesquisarServicePort {
 
   @Autowired
-  private IQRCodeGenerator qrCodeGenerator;
-
-  @Autowired
-  private IPagamentoRepositoryPort.GetRepository pagamentoGetRepository;
+  private IPagamentoPesquisarRepositoryPort pagamentoPesquisarRepository;
 
   @Override
   public Page<PagamentoModel> pesquisar(final PagamentoFiltro filtro, final Pageable paginacao) {
 
     return Optional.of(filtro)
-      .map(parametrosDePesquisa -> this.pagamentoGetRepository.pesquisar(parametrosDePesquisa, paginacao))
+      .map(parametrosDePesquisa -> this.pagamentoPesquisarRepository.pesquisar(parametrosDePesquisa, paginacao))
       .orElseThrow();
-  }
-
-  @Override
-  public InputStreamResource buscarQrCodePorId(final Long id) {
-
-    return this.pagamentoGetRepository.consultarPorId(id)
-      .map(model -> {
-        var inputStream = this.qrCodeGenerator.recuperarImagemQrCode(model.getNomeImagemQRCode());
-       return new InputStreamResource(inputStream);
-      })
-      .orElseThrow(() -> {
-        log.info(String.format(MensagemPadrao.PAGAMENTO_NAO_ENCONTRADO, id));
-        throw new PagamentoNaoEncontradoException(id);
-      });
   }
 }
 
