e6f5d2833c517cac58d69f430a65c44355f9d076
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-16 07:00:01 -0400

    FEAT: add exceção de CPF inválido.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/exceptions/MensagemPadrao.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/exceptions/MensagemPadrao.java
@@ -6,6 +6,8 @@ public final class MensagemPadrao {
 
   public static final String CPF_NAO_ENCONTRADO = "O Cliente, com CPF %s, não foi encontrado.";
 
+  public static final String CPF_INVALIDO = "O CPF, de número %s, é inválido.";
+
   public static final String MEDIA_NAO_SUPORTADA = "O recurso não suporta esse tipo de midia.";
 
   public static final String CLIENTE_NAO_ENCONTRADO = "O Cliente, com id %s, não foi encontrado!";
