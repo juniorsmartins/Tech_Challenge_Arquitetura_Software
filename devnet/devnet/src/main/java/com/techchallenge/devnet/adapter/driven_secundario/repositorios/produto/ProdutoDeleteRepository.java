70f0e7b84e3843f770c275a935ba2084927a8382
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-20 07:37:06 -0400

    REFACTOR: adequação de Repository de Produto.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven_secundario/repositorios/produto/ProdutoDeleteRepository.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven_secundario/repositorios/produto/ProdutoDeleteRepository.java
@@ -2,16 +2,17 @@ package com.techchallenge.devnet.adapter.driven_secundario.repositorios.produto;
 
 import com.techchallenge.devnet.adapter.driven_secundario.adapter_saida.IAdapterSaida;
 import com.techchallenge.devnet.adapter.driven_secundario.entities.ProdutoEntity;
-import com.techchallenge.devnet.core.application.ports.saida.produto.IProdutoSalvarRepositoryPort;
+import com.techchallenge.devnet.core.application.ports.saida.produto.IProdutoApagarRepositoryPort;
 import com.techchallenge.devnet.core.domain.models.ProdutoModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
-public class ProdutoSalvarRepositoryAdapter implements IProdutoSalvarRepositoryPort {
+public class ProdutoDeleteRepository implements IProdutoApagarRepositoryPort {
 
   @Autowired
   private IAdapterSaida mapper;
@@ -19,14 +20,16 @@ public class ProdutoSalvarRepositoryAdapter implements IProdutoSalvarRepositoryP
   @Autowired
   private ProdutoRepositoryJpa jpa;
 
-  @Transactional
+  @Transactional(isolation = Isolation.SERIALIZABLE)
   @Override
-  public ProdutoModel salvar(final ProdutoModel produtoModel) {
+  public void deletar(final ProdutoModel produtoModel) {
 
-    return Optional.of(produtoModel)
-      .map(model -> this.mapper.converterOrigemParaDestino(model, ProdutoEntity.class))
-      .map(this.jpa::saveAndFlush)
-      .map(entity -> this.mapper.converterOrigemParaDestino(entity, ProdutoModel.class))
+    Optional.of(produtoModel)
+      .map(model -> {
+        var produtoEntity = this.mapper.converterOrigemParaDestino(model, ProdutoEntity.class);
+        this.jpa.delete(produtoEntity);
+        return true;
+      })
       .orElseThrow();
   }
 }
--- devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven_secundario/repositorios/produto/ProdutoPostRepository.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/adapter/driven_secundario/repositorios/produto/ProdutoPostRepository.java
@@ -2,17 +2,16 @@ package com.techchallenge.devnet.adapter.driven_secundario.repositorios.produto;
 
 import com.techchallenge.devnet.adapter.driven_secundario.adapter_saida.IAdapterSaida;
 import com.techchallenge.devnet.adapter.driven_secundario.entities.ProdutoEntity;
-import com.techchallenge.devnet.core.application.ports.saida.produto.IProdutoApagarRepositoryPort;
+import com.techchallenge.devnet.core.application.ports.saida.produto.IProdutoSalvarRepositoryPort;
 import com.techchallenge.devnet.core.domain.models.ProdutoModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
-public class ProdutoApagarRepositoryAdapter implements IProdutoApagarRepositoryPort {
+public class ProdutoPostRepository implements IProdutoSalvarRepositoryPort {
 
   @Autowired
   private IAdapterSaida mapper;
@@ -20,16 +19,14 @@ public class ProdutoApagarRepositoryAdapter implements IProdutoApagarRepositoryP
   @Autowired
   private ProdutoRepositoryJpa jpa;
 
-  @Transactional(isolation = Isolation.SERIALIZABLE)
+  @Transactional
   @Override
-  public void deletar(final ProdutoModel produtoModel) {
+  public ProdutoModel salvar(final ProdutoModel produtoModel) {
 
-    Optional.of(produtoModel)
-      .map(model -> {
-        var produtoEntity = this.mapper.converterOrigemParaDestino(model, ProdutoEntity.class);
-        this.jpa.delete(produtoEntity);
-        return true;
-      })
+    return Optional.of(produtoModel)
+      .map(model -> this.mapper.converterOrigemParaDestino(model, ProdutoEntity.class))
+      .map(this.jpa::saveAndFlush)
+      .map(entity -> this.mapper.converterOrigemParaDestino(entity, ProdutoModel.class))
       .orElseThrow();
   }
 }
