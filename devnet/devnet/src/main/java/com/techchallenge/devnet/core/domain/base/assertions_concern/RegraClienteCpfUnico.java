51f40ce7f786f867c8e0c15cec695bc7a11c73eb
Junior Martins <juniorsoaresmartins@gmail.com>
2023-07-23 16:03:35 -0400

    REFACTOR: troca de @Autowired por injeção no Construtor.

--- devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/assertions_concern/RegraClienteCpfUnico.java
+++ devnet/devnet/src/main/java/com/techchallenge/devnet/core/domain/base/assertions_concern/RegraClienteCpfUnico.java
@@ -3,19 +3,21 @@ package com.techchallenge.devnet.core.domain.base.assertions_concern;
 import com.techchallenge.devnet.core.application.ports.saida.cliente.IClienteConsultarPorCpfRepositoryPort;
 import com.techchallenge.devnet.core.domain.base.exceptions.http_409.RegraClienteCpfUnicoException;
 import com.techchallenge.devnet.core.domain.models.ClienteModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class RegraClienteCpfUnico implements RegrasCliente {
 
-  @Autowired
-  private IClienteConsultarPorCpfRepositoryPort clienteConsultarPorCpfRepository;
+  private final IClienteConsultarPorCpfRepositoryPort repositorio;
+
+  public RegraClienteCpfUnico(IClienteConsultarPorCpfRepositoryPort repositorio) {
+    this.repositorio = repositorio;
+  }
 
   @Override
   public void executar(ClienteModel clienteModel) {
 
-    var existeCpfIgual = this.clienteConsultarPorCpfRepository.consultarPorCpf(clienteModel.getCpf())
+    var existeCpfIgual = this.repositorio.consultarPorCpf(clienteModel.getCpf())
       .filter(cliente -> !cliente.getId().equals(clienteModel.getId()))
       .isPresent();
 
